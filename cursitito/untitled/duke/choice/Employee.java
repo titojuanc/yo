aa59b79425999e5e0ecf1c26ed6086fce8fdc34b
titojuanc <167209613+titojuanc@users.noreply.github.com>
2025-05-25 23:15:22 -0300

    Terminado
    Practical Object-Oriented Concepts: Part 2

---
+++ cursitito/untitled/duke/choice/Employee.java
@@ -0,0 +1,25 @@
+public class Employee{
+    private String nombre;
+    private Departamento departamento;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    public Employee(String nombre, Departamento departamento) {
+        this.nombre = nombre;
+        this.departamento = departamento;
+    }
+  
+
+
+
+
+}
---
+++ cursitito/untitled/duke/choice/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Employee> employeesD1 = new ArrayList<>();
+        ArrayList<Employee> employeesD2 = new ArrayList<>();
+        Departamento d1 = new Departamento("Departamento 1", employeesD1);
+        Departamento d2 = new Departamento("Departamento 2", employeesD2);
+        
+        Employee e1 = new Employee("Julian", d1);
+        Employee e2 = new Employee("Mario", d1);
+        Employee e3 = new Employee("Francesca", d2);
+        
+        employeesD1.add(e1);
+        employeesD1.add(e2);
+        employeesD2.add(e3);
+        
+        d1.asignarDepartamentoEmpleado(e1, d1);
+        d1.asignarDepartamentoEmpleado(e3, d1);
+        d1.mostrarEmpleadosPorDepartamento();
+        d2.mostrarEmpleadosPorDepartamento();
+    }
+}
